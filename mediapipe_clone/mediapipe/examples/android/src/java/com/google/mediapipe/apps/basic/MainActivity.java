f002acdf086fa2dd788f1383dccf7885a1fc1de2
Sumanth Aluri <sumanth.phi2000@gmail.com>
2021-11-06 11:46:23 -0400

    added mediapipe_clone with our app's code

---
+++ mediapipe_clone/mediapipe/examples/android/src/java/com/google/mediapipe/apps/basic/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.phantomouse;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import android.R;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BleMouse mouse = new BleMouse(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mouse.getProxy();
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameL,
+                    new Sc1class()).commit();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_bluetooth:
+                            selectedFragment = new Sc1class();
+                            break;
+                        case R.id.nav_callibrate:
+                            selectedFragment = new Sc2class();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new Sc3class(mouse);
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameL,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+}
