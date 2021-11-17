02ff109314ba388af55fbdcef150e1de45a60ef4
manthi4 <sumanth.phi2000@gmail.com>
2021-11-16 21:28:32 -0500

    app compiles and preview works

--- mediapipe_clone/mediapipe/examples/android/src/java/com/google/mediapipe/apps/basic/Sc1class.java
+++ mediapipe_clone/mediapipe/examples/android/src/java/com/google/mediapipe/apps/basic/Sc1class.java
@@ -23,35 +23,95 @@ import androidx.lifecycle.LifecycleOwner;
 // import android.R;
 
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.SurfaceTexture;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Size;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.google.mediapipe.components.CameraHelper;
+import com.google.mediapipe.components.CameraXPreviewHelper;
+import com.google.mediapipe.components.ExternalTextureConverter;
+import com.google.mediapipe.components.FrameProcessor;
+import com.google.mediapipe.components.PermissionHelper;
+import com.google.mediapipe.framework.AndroidAssetUtil;
+import com.google.mediapipe.glutil.EglManager;
+
+
+
+
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.concurrent.ExecutionException;
 
 
 public class Sc1class extends Fragment {
+    private static final String TAG = "S1ClassFrag";
     // PreviewView previewView;
 
+    private static final boolean FLIP_FRAMES_VERTICALLY = true;
+    private static final int NUM_BUFFERS = 2;
+    static {
+        // Load all native libraries needed by the app.
+        System.loadLibrary("mediapipe_jni");
+        try {
+        System.loadLibrary("opencv_java3");
+        } catch (java.lang.UnsatisfiedLinkError e) {
+        // Some example apps (e.g. template matching) require OpenCV 4.
+        System.loadLibrary("opencv_java4");
+        }
+    }
+
+    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
+    // frames onto a {@link Surface}.
+    protected FrameProcessor processor;
+    // Handles camera access via the {@link CameraX} Jetpack support library.
+    protected CameraXPreviewHelper cameraHelper;
+
+    // {@link SurfaceTexture} where the camera-preview frames can be accessed.
+    private SurfaceTexture previewFrameTexture;
+    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
+    private SurfaceView previewDisplayView;
+
+    // Creates and manages an {@link EGLContext}.
+    private EglManager eglManager;
+    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
+    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
+    private ExternalTextureConverter converter;
+
+    // ApplicationInfo for retrieving metadata defined in the manifest.
+    private ApplicationInfo applicationInfo;
+
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    protected int getContentViewLayoutResId() {
+        return R.layout.screen1;
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
 
-        cameraProviderFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                // bindPreview(cameraProvider);
-            } catch (ExecutionException | InterruptedException e) {
-                // No errors need to be handled for this Future.
-                // This should never be reached.
-            }
-        }, ContextCompat.getMainExecutor(getContext()));
+        try {
+        applicationInfo =
+            getActivity().getPackageManager().getApplicationInfo(getActivity().getPackageName(), PackageManager.GET_META_DATA);
+        } catch (NameNotFoundException e) {
+            Log.e(TAG, "Cannot find application info: " + e);
+        }
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       
+
+       
         return inflater.inflate(R.layout.screen1, container, false);
     }
 
@@ -59,26 +119,129 @@ public class Sc1class extends Fragment {
     public void onStart() {
         super.onStart();
 
-        // previewView = getView().findViewById(R.id.previewView);
+       previewDisplayView = new SurfaceView(getContext());
+        setupPreviewDisplayView();
 
-        // Request camera permissions
+            // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(getContext());
+        eglManager = new EglManager(null);
+        processor =
+            new FrameProcessor(
+                getContext(),
+                eglManager.getNativeContext(),
+                applicationInfo.metaData.getString("binaryGraphName"),
+                applicationInfo.metaData.getString("inputVideoStreamName"),
+                applicationInfo.metaData.getString("outputVideoStreamName"));
+        processor
+            .getVideoSurfaceOutput()
+            .setFlipY(
+                applicationInfo.metaData.getBoolean("flipFramesVertically", FLIP_FRAMES_VERTICALLY));
 
-//        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
-//        }
+        PermissionHelper.checkAndRequestCameraPermissions(getActivity());
     }
 
-    // void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
-    //     Preview preview = new Preview.Builder()
-    //             .build();
 
-    //     CameraSelector cameraSelector = new CameraSelector.Builder()
-    //             .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-    //             .build();
+    @Override
+    public void onResume() {
+        super.onResume();
+        converter =
+            new ExternalTextureConverter(
+                eglManager.getContext(),
+                applicationInfo.metaData.getInt("converterNumBuffers", NUM_BUFFERS));
+        converter.setFlipY(
+            applicationInfo.metaData.getBoolean("flipFramesVertically", FLIP_FRAMES_VERTICALLY));
+        converter.setConsumer(processor);
+        if (PermissionHelper.cameraPermissionsGranted(getActivity())) {
+        startCamera();
+        }
+    }
 
-    //     preview.setSurfaceProvider(previewView.getSurfaceProvider());
+    @Override
+    public void onPause() {
+        super.onPause();
+        converter.close();
 
-    //     Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
-    // }
+        // Hide preview display until we re-open the camera again.
+        previewDisplayView.setVisibility(View.GONE);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(
+        int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    protected void onCameraStarted(SurfaceTexture surfaceTexture) {
+        previewFrameTexture = surfaceTexture;
+        // Make the display view visible to start showing the preview. This triggers the
+        // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+        previewDisplayView.setVisibility(View.VISIBLE);
+    }
+
+    protected Size cameraTargetResolution() {
+        return null; // No preference and let the camera (helper) decide.
+    }
+
+    public void startCamera() {
+        cameraHelper = new CameraXPreviewHelper();
+        previewFrameTexture = converter.getSurfaceTexture();
+        cameraHelper.setOnCameraStartedListener(
+            surfaceTexture -> {
+            onCameraStarted(surfaceTexture);
+            });
+        CameraHelper.CameraFacing cameraFacing =
+            applicationInfo.metaData.getBoolean("cameraFacingFront", false)
+                ? CameraHelper.CameraFacing.FRONT
+                : CameraHelper.CameraFacing.BACK;
+        cameraHelper.startCamera(
+            getActivity(), cameraFacing, previewFrameTexture, cameraTargetResolution());
+    }
+
+    protected Size computeViewSize(int width, int height) {
+        return new Size(width, height);
+    }
+
+    protected void onPreviewDisplaySurfaceChanged(
+        SurfaceHolder holder, int format, int width, int height) {
+        // (Re-)Compute the ideal size of the camera-preview display (the area that the
+        // camera-preview frames get rendered onto, potentially with scaling and rotation)
+        // based on the size of the SurfaceView that contains the display.
+        Size viewSize = computeViewSize(width, height);
+        Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
+        boolean isCameraRotated = cameraHelper.isCameraRotated();
+
+        // Configure the output width and height as the computed display size.
+        converter.setDestinationSize(
+            isCameraRotated ? displaySize.getHeight() : displaySize.getWidth(),
+            isCameraRotated ? displaySize.getWidth() : displaySize.getHeight());
+    }
+
+  private void setupPreviewDisplayView() {
+    previewDisplayView.setVisibility(View.GONE);
+    ViewGroup viewGroup = getView().findViewById(R.id.preview_display_layout);
+    viewGroup.addView(previewDisplayView);
+
+    previewDisplayView
+        .getHolder()
+        .addCallback(
+            new SurfaceHolder.Callback() {
+              @Override
+              public void surfaceCreated(SurfaceHolder holder) {
+                processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
+              }
+
+              @Override
+              public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                onPreviewDisplaySurfaceChanged(holder, format, width, height);
+              }
+
+              @Override
+              public void surfaceDestroyed(SurfaceHolder holder) {
+                processor.getVideoSurfaceOutput().setSurface(null);
+              }
+            });
+  }
 
 }
